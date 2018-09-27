4c726088553c6cab846eba8bfa12cfeb980a32db
alexstepanyuk <alex-stepanyuk@yandex.ru>
2018-09-27 16:49:15 +0700

    Test-134092 Selenium: Supporting tests for 8.93 version

--- src/main/java/com/onevizion/uitest/api/helper/formdesigner/FormDesignerJs.java
+++ src/main/java/com/onevizion/uitest/api/helper/formdesigner/FormDesignerJs.java
@@ -26,17 +26,17 @@ class FormDesignerJs extends Js {
 
     List<FormDesignerField> getElementsOnForm() {
         String resultStr = (String) execJs2("" + 
-                "var formDesigner1 = getFormDesigner();" + 
+                "var formDesigner = getFormDesigner();" + 
                 "var result = \"\";" + 
                 "result = result + \"[\";" + 
-                "for (var key in  formDesigner1.selectedValues) {" + 
+                "for (var key in  formDesigner.selectedValues) {" + 
                 "    result = result + \"{\";" + 
-                "    result = result + \"\\\"id\\\": \\\"\" + formDesigner1.selectedValues[key].getId() + \"\\\", \";" + 
-                "    result = result + \"\\\"name\\\": \\\"\" + formDesigner1.selectedValues[key].getName() + \"\\\", \";" + 
-                "    result = result + \"\\\"label\\\": \\\"\" + formDesigner1.selectedValues[key].getLbl() + \"\\\", \";" + 
-                "    result = result + \"\\\"prefix\\\": \\\"\" + formDesigner1.selectedValues[key].getPrefLbl() + \"\\\", \";" + 
-                "    result = result + \"\\\"row\\\": \\\"\" + formDesigner1.selectedValues[key].getRow() + \"\\\", \";" + 
-                "    result = result + \"\\\"col\\\": \\\"\" + formDesigner1.selectedValues[key].getCol() + \"\\\"\";" + 
+                "    result = result + \"\\\"id\\\": \\\"\" + formDesigner.selectedValues[key].getId() + \"\\\", \";" + 
+                "    result = result + \"\\\"name\\\": \\\"\" + formDesigner.selectedValues[key].getName() + \"\\\", \";" + 
+                "    result = result + \"\\\"label\\\": \\\"\" + formDesigner.selectedValues[key].getLbl() + \"\\\", \";" + 
+                "    result = result + \"\\\"prefix\\\": \\\"\" + formDesigner.selectedValues[key].getPrefLbl() + \"\\\", \";" + 
+                "    result = result + \"\\\"row\\\": \\\"\" + formDesigner.selectedValues[key].getRow() + \"\\\", \";" + 
+                "    result = result + \"\\\"col\\\": \\\"\" + formDesigner.selectedValues[key].getCol() + \"\\\"\";" + 
                 "    result = result + \"},\";" + 
                 "}" + 
                 "result = result.substring(0, result.length - 1);" + 
