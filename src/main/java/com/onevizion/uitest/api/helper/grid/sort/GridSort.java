656b1bb7fdfb90cecde70d64a181f2818f1d04ce
George Kovalev <kgp.88@mail.ru>
2018-12-04 09:54:35 +0700

    Test-136414 Selenium: create GridSort API

--- src/main/java/com/onevizion/uitest/api/helper/grid/sort/GridSort.java
+++ src/main/java/com/onevizion/uitest/api/helper/grid/sort/GridSort.java
@@ -1,9 +1,14 @@
 package com.onevizion.uitest.api.helper.grid.sort;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
@@ -16,6 +21,7 @@ import com.onevizion.uitest.api.helper.Element;
 import com.onevizion.uitest.api.helper.ElementWait;
 import com.onevizion.uitest.api.helper.Js;
 import com.onevizion.uitest.api.helper.Wait;
+import com.onevizion.uitest.api.vo.GridColumnType;
 import com.onevizion.uitest.api.vo.SortType;
 
 @Component
@@ -78,20 +84,15 @@ public class GridSort {
 
         wait.waitGridLoad(gridId, gridId);
 
-        //List<Object> elements = (List<Object>) js.getGridSort(gridId); //TODO
-        //Assert.assertEquals((Long) elements.get(0), columnIndex, "Sorting working is not correct"); //TODO
-        //Assert.assertEquals((String) elements.get(1), "asc", "Sorting working is not correct"); //TODO
+        @SuppressWarnings("unchecked")
+        List<Object> elements = (List<Object>) js.getGridSort(gridId);
+        Assert.assertEquals((Long) elements.get(0), columnIndex, "Sorting working is not correct");
+        Assert.assertEquals((String) elements.get(1), sortType.getTypeString(), "Sorting working is not correct");
 
         String sortColumnId = js.getGridSortColumnIdByGridId(gridId);
         Assert.assertEquals(sortColumnId, columnId);
         String gridSortType = js.getGridSortTypeByGridId(gridId);
-        if (SortType.ASC.equals(sortType)) {
-            Assert.assertEquals(gridSortType, "0");
-        } else if (SortType.DESC.equals(sortType)) {
-            Assert.assertEquals(gridSortType, "1");
-        } else {
-            throw new SeleniumUnexpectedException("Not support SortType. SortType=" + sortType);
-        }
+        Assert.assertEquals(gridSortType, sortType.getTypeNumber());
     }
 
     public void sortColumn(SortType sortType, String columnLabel, String columnLabel2) {
@@ -131,20 +132,15 @@ public class GridSort {
 
         wait.waitGridLoad(gridId, gridId);
 
-        //List<Object> elements = (List<Object>) js.getGridSort(gridId); //TODO
-        //Assert.assertEquals((Long) elements.get(0), columnIndex, "Sorting working is not correct"); //TODO
-        //Assert.assertEquals((String) elements.get(1), "asc", "Sorting working is not correct"); //TODO
+        @SuppressWarnings("unchecked")
+        List<Object> elements = (List<Object>) js.getGridSort(gridId);
+        Assert.assertEquals((Long) elements.get(0), columnIndex, "Sorting working is not correct");
+        Assert.assertEquals((String) elements.get(1), sortType.getTypeString(), "Sorting working is not correct");
 
         String sortColumnId = js.getGridSortColumnIdByGridId(gridId);
         Assert.assertEquals(sortColumnId, columnId);
         String gridSortType = js.getGridSortTypeByGridId(gridId);
-        if (SortType.ASC.equals(sortType)) {
-            Assert.assertEquals(gridSortType, "0");
-        } else if (SortType.DESC.equals(sortType)) {
-            Assert.assertEquals(gridSortType, "1");
-        } else {
-            throw new SeleniumUnexpectedException("Not support SortType. SortType=" + sortType);
-        }
+        Assert.assertEquals(gridSortType, sortType.getTypeNumber());
     }
 
     public void checkColumnSortAvailable(String columnLabel) {
@@ -185,6 +181,131 @@ public class GridSort {
         Long columnIndex = js.getColumnIndexByLabel(gridId, columnLabel, columnLabel2);
         String columnSortType = gridSortJs.getColumnSortType(gridId, columnIndex);
         Assert.assertEquals(columnSortType, COLUMN_SORT_TYPE_NA);
+    }
+
+
+
+
+
+
+
+    public void checkColumn(SortType sortType, GridColumnType gridColumnType, String columnLabel) {
+        checkColumn(AbstractSeleniumCore.getGridIdx(), sortType, gridColumnType, columnLabel);
+    }
+
+    public void checkColumn(Long gridId, SortType sortType, GridColumnType gridColumnType, String columnLabel) {
+        Long columnIndex = js.getColumnIndexByLabel(gridId, columnLabel);
+
+        Long rowsNum = js.getGridRowsCount(gridId);
+
+        if (rowsNum <= 1L) {
+            return; //Nothing to check
+            //TODO may be log with error or throw exception?
+        }
+
+        Long curRowNum = 1L;
+        @SuppressWarnings("unchecked")
+        List<String> vals = (List<String>) js.getGridCellsValuesForColumnByColIndex(gridId, rowsNum, columnIndex);
+        while (curRowNum < rowsNum) {
+            String curVal = vals.get(curRowNum.intValue());
+            if (StringUtils.isNotBlank(curVal)) {
+                curVal = curVal.replaceAll("^<[aA].*?>", "").replaceAll("</[aA]>$", ""); /*Example: condition for link*/
+                curVal = StringUtils.substringBefore(curVal, "\n"); /*Example: condition for pl/sql block where value may have character of new line*/
+            }
+
+            String prevVal = vals.get(curRowNum.intValue() - 1);
+            if (StringUtils.isNotBlank(prevVal)) {
+                prevVal = prevVal.replaceAll("^<[aA].*?>", "").replaceAll("</[aA]>$", ""); /*Example: condition for link*/
+                prevVal = StringUtils.substringBefore(prevVal, "\n"); /*Example: condition for pl/sql block where value may have character of new line*/
+            }
+
+            if ("".equals(curVal) || "".equals(prevVal)) {
+                
+            } else {
+                if (GridColumnType.NUMBER.equals(gridColumnType)) {
+                    checkColumnNumber(curVal, prevVal, sortType, columnIndex, curRowNum);
+                } else if (GridColumnType.TIMESTAMP.equals(gridColumnType)) {
+                    checkColumnTimestamp(curVal, prevVal, sortType, columnIndex, curRowNum);
+                } else if (GridColumnType.BOOLEAN.equals(gridColumnType)) {
+                    checkColumnBoolean(curVal, prevVal, sortType, columnIndex, curRowNum);
+                } else if (GridColumnType.TEXT.equals(gridColumnType)) {
+                    //TODO check sort by text column not working. because oracle and java sort string by different logic
+                } else if (GridColumnType.DATE.equals(gridColumnType)) {
+                    checkColumnDate(curVal, prevVal, sortType, columnIndex, curRowNum);
+                } else {
+                    throw new SeleniumUnexpectedException("Not support GridColumnType. GridColumnType=" + gridColumnType);
+                }
+            }
+
+            curRowNum = curRowNum + 1L;
+        }
+    }
+
+    private void checkColumnNumber(String curVal, String prevVal, SortType sortType, Long columnIdx, Long curRowNum) {
+        Long curValNum = Long.valueOf(curVal);
+        Long prevValNum = Long.valueOf(prevVal);
+
+        if (SortType.ASC.equals(sortType)) {
+            Assert.assertEquals(prevValNum.compareTo(curValNum) <= 0, true, "Sort asc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curValNum + "], prev row num [" + (curRowNum - 1) + "] val [" + prevValNum +"]");
+        } else if (SortType.DESC.equals(sortType)) {
+            Assert.assertEquals(curValNum.compareTo(prevValNum) <= 0, true, "Sort desc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curValNum + "], prev row num [" + (curRowNum - 1) + "] val [" + prevValNum +"]");
+        } else {
+            throw new SeleniumUnexpectedException("Not support SortType. SortType=" + sortType);
+        }
+    }
+
+    private void checkColumnTimestamp(String curVal, String prevVal, SortType sortType, Long columnIdx, Long curRowNum) {
+        //TODO Date format should depend on user Settings
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+        Date curValDate;
+        Date prevValDate;
+        try {
+            curValDate = dateFormat.parse(curVal);
+            prevValDate = dateFormat.parse(prevVal);
+        } catch (ParseException e) {
+            throw new SeleniumUnexpectedException("Exception while parsing date value for [" + columnIdx + "] column. Cur row num [" + curRowNum +"] val [" + curVal + "], prev row num [" + (curRowNum - 1) + "] val [" + prevVal +"]");
+        }
+
+        if (SortType.ASC.equals(sortType)) {
+            Assert.assertEquals(prevValDate.compareTo(curValDate) <= 0, true, "Sort asc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curValDate + "], prev row num [" + (curRowNum - 1) + "] val [" + prevValDate +"]");
+        } else if (SortType.DESC.equals(sortType)) {
+            Assert.assertEquals(curValDate.compareTo(prevValDate) <= 0, true, "Sort desc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curValDate + "], prev row num [" + (curRowNum - 1) + "] val [" + prevValDate +"]");
+        } else {
+            throw new SeleniumUnexpectedException("Not support SortType. SortType=" + sortType);
+        }
+    }
+
+    private void checkColumnDate(String curVal, String prevVal, SortType sortType, Long columnIdx, Long curRowNum) {
+        //TODO Date format should depend on user Settings
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+
+        Date curValDate;
+        Date prevValDate;
+        try {
+            curValDate = dateFormat.parse(curVal);
+            prevValDate = dateFormat.parse(prevVal);
+        } catch (ParseException e) {
+            throw new SeleniumUnexpectedException("Exception while parsing date value for [" + columnIdx + "] column. Cur row num [" + curRowNum +"] val [" + curVal + "], prev row num [" + (curRowNum - 1) + "] val [" + prevVal +"]");
+        }
+
+        if (SortType.ASC.equals(sortType)) {
+            Assert.assertEquals(prevValDate.compareTo(curValDate) <= 0, true, "Sort asc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curValDate + "], prev row num [" + (curRowNum - 1) + "] val [" + prevValDate +"]");
+        } else if (SortType.DESC.equals(sortType)) {
+            Assert.assertEquals(curValDate.compareTo(prevValDate) <= 0, true, "Sort desc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curValDate + "], prev row num [" + (curRowNum - 1) + "] val [" + prevValDate +"]");
+        } else {
+            throw new SeleniumUnexpectedException("Not support SortType. SortType=" + sortType);
+        }
+    }
+
+    private void checkColumnBoolean(String curVal, String prevVal, SortType sortType, Long columnIdx, Long curRowNum) {
+        if (SortType.ASC.equals(sortType)) {
+            Assert.assertEquals(prevVal.compareTo(curVal) <= 0, true, "Sort asc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curVal + "], prev row num [" + (curRowNum - 1) + "] val [" + prevVal +"]");
+        } else if (SortType.DESC.equals(sortType)) {
+            Assert.assertEquals(curVal.compareTo(prevVal) <= 0, true, "Sort desc for [" + columnIdx + "] column is wrong. Cur row num [" + curRowNum +"] val [" + curVal + "], prev row num [" + (curRowNum - 1) + "] val [" + prevVal +"]");
+        } else {
+            throw new SeleniumUnexpectedException("Not support SortType. SortType=" + sortType);
+        }
     }
 
 }
--- src/main/java/com/onevizion/uitest/api/vo/SortType.java
+++ src/main/java/com/onevizion/uitest/api/vo/SortType.java
@@ -2,17 +2,29 @@ package com.onevizion.uitest.api.vo;
 
 public enum SortType {
 
-    ASC("Sort ASC"),
-    DESC("Sort DESC");
+    ASC("Sort ASC", "asc", "0"),
+    DESC("Sort DESC", "des", "1");
 
     private String name;
+    private String typeString;
+    private String typeNumber;
 
-    private SortType(String name) {
+    private SortType(String name, String typeString, String typeNumber) {
         this.name = name;
+        this.typeString = typeString;
+        this.typeNumber = typeNumber;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getTypeString() {
+        return typeString;
+    }
+
+    public String getTypeNumber() {
+        return typeNumber;
     }
 
 }
