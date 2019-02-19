08bb4a1c9f4f791e7772d7b271444819bc263838
George Kovalev <kgp.88@mail.ru>
2019-02-19 11:06:24 +0700

    Test-123733 Selenium: create tests for check filters by task dates on TB page

---
+++ src/main/java/com/onevizion/uitest/api/helper/userpage/filter/TbTaskDateOnTbPage.java
@@ -0,0 +1,64 @@
+package com.onevizion.uitest.api.helper.userpage.filter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+import com.onevizion.uitest.api.helper.Grid;
+import com.onevizion.uitest.api.helper.Js;
+import com.onevizion.uitest.api.vo.ConfigFieldType;
+import com.onevizion.uitest.api.vo.FilterOperatorType;
+
+@Component
+public class TbTaskDateOnTbPage {
+
+    @Resource
+    private Js js;
+
+    @Resource
+    private Grid grid;
+
+    @Resource
+    private UserpageFilter userpageFilter;
+
+    @SuppressWarnings("unchecked")
+    public void test(String columnId, String value, String startFinish) {
+        Long columnIndex = js.getGridColIndexById(0L, columnId);
+
+        String fieldName = js.getGridColumnLabelByColIndex(0L, columnIndex, 0L);
+        if ("F".equals(startFinish)) {
+            columnIndex = columnIndex + 1L;
+        }
+
+        Long rowsCnt = grid.getGridRowsCount(0L);
+        List<String> cellVals = (List<String>) js.getGridCellsValuesTxtForColumnByColIndex(0L, rowsCnt, columnIndex);
+
+        List<FilterOperatorType> operators = FilterOperatorType.getTaskDateOperators();
+        userpageFilter.checkFilterOperators(fieldName, Arrays.asList("S", "F"), operators);
+
+        userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.EQUAL, ConfigFieldType.DATE, columnIndex, null, cellVals, null);
+        userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.MORE, ConfigFieldType.DATE, columnIndex, null, cellVals, null);
+        userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.LESS, ConfigFieldType.DATE, columnIndex, null, cellVals, null);
+        userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.MORE_AND_EQUAL, ConfigFieldType.DATE, columnIndex, null, cellVals, null);
+        userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.LESS_AND_EQUAL, ConfigFieldType.DATE, columnIndex, null, cellVals, null);
+        //TODO
+        //>=Today
+        //<=Today
+        //Within
+        //This Wk
+        //This Wk to Dt
+        //This Mo
+        //This Mo to Dt
+        //This FQ
+        //This FQ to Dt
+        //This FY
+        //This FY to Dt
+        //userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.NOT_EQUAL, ConfigFieldType.DATE, columnIndex, null, cellVals, null); //TODO BUG
+        userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.NULL, ConfigFieldType.DATE, columnIndex, null, cellVals, null);
+        userpageFilter.checkFilter(fieldName, null, value, startFinish, FilterOperatorType.NOT_NULL, ConfigFieldType.DATE, columnIndex, null, cellVals, null);
+    }
+
+}
