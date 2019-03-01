f8c4c4c697666b4a039a550f5f9761f07871ecd1
George Kovalev <kgp.88@mail.ru>
2019-03-01 13:40:43 +0700

    Test-142867 Selenium: new tests for filter with trackor key with text value

---
+++ src/main/java/com/onevizion/uitest/api/helper/userpage/filter/TbXitorKeyByText.java
@@ -0,0 +1,43 @@
+package com.onevizion.uitest.api.helper.userpage.filter;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+import com.onevizion.uitest.api.helper.Grid;
+import com.onevizion.uitest.api.helper.Js;
+import com.onevizion.uitest.api.vo.FilterOperatorType;
+
+@Component
+public class TbXitorKeyByText {
+
+    @Resource
+    private Js js;
+
+    @Resource
+    private Grid grid;
+
+    @Resource
+    private UserpageFilter userpageFilter;
+
+    @SuppressWarnings("unchecked")
+    public void test(String columnId, String value, boolean supportOuterOperations, List<String> ... cellValsKeys) {
+        Long columnIndex = js.getGridColIndexById(0L, columnId);
+
+        String fieldName = js.getGridColumnLabelByColIndex(0L, columnIndex, 0L);
+
+        Long rowsCnt = grid.getGridRowsCount(0L);
+        List<String> cellVals = (List<String>) js.getGridCellsValuesTxtForColumnByColIndex(0L, rowsCnt, columnIndex);
+
+        userpageFilter.checkFilterTrackorSelectorByText(fieldName, FilterOperatorType.EQUAL, value, columnIndex, cellVals);
+        userpageFilter.checkFilterTrackorSelectorByText(fieldName, FilterOperatorType.NOT_EQUAL, value, columnIndex, cellVals);
+
+        if (supportOuterOperations) {
+            userpageFilter.checkFilterTrackorSelectorByText(fieldName, FilterOperatorType.EQUAL_AND_EMPTY_FOR_OTHER, value, columnIndex, cellVals, cellValsKeys);
+            userpageFilter.checkFilterTrackorSelectorByText(fieldName, FilterOperatorType.NOT_EQUAL_AND_EMPTY_FOR_OTHER, value, columnIndex, cellVals, cellValsKeys);
+        }
+    }
+
+}
