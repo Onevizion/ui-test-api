8850d6cdb2bd69e38ac164521a5194ab080892e1
George Kovalev <kgp.88@mail.ru>
2019-03-08 10:59:33 +0700

    Test-140488 Selenium: check partial input for date fields

--- src/main/java/com/onevizion/uitest/api/vo/DateFormatType.java
+++ src/main/java/com/onevizion/uitest/api/vo/DateFormatType.java
@@ -2,23 +2,35 @@ package com.onevizion.uitest.api.vo;
 
 public enum DateFormatType {
 
-    MM_DD_YYYY_SLASH("MM/DD/YYYY"),
-    DD_MM_YYYY_SLASH("DD/MM/YYYY"),
-    MM_DD_YYYY_DOT("MM.DD.YYYY"),
-    DD_MM_YYYY_DOT("DD.MM.YYYY"),
-    MM_DD_YY_SLASH("MM/DD/YY"),
-    DD_MM_YY_SLASH("DD/MM/YY"),
-    MM_DD_YY_DOT("MM.DD.YY"),
-    DD_MM_YY_DOT("DD.MM.YY");
+    MM_DD_YYYY_SLASH("MM/DD/YYYY", "MM/dd/yyyy", "/"),
+    DD_MM_YYYY_SLASH("DD/MM/YYYY", "dd/MM/yyyy", "/"),
+    MM_DD_YYYY_DOT("MM.DD.YYYY", "MM.dd.yyyy", "."),
+    DD_MM_YYYY_DOT("DD.MM.YYYY", "dd.MM.yyyy", "."),
+    MM_DD_YY_SLASH("MM/DD/YY", "MM/dd/yy", "/"),
+    DD_MM_YY_SLASH("DD/MM/YY", "dd/MM/yy", "/"),
+    MM_DD_YY_DOT("MM.DD.YY", "MM.dd.yy", "."),
+    DD_MM_YY_DOT("DD.MM.YY", "dd.MM.yy", ".");
 
     private String format;
+    private String javaFormat;
+    private String separator;
 
-    private DateFormatType(String format) {
+    private DateFormatType(String format, String javaFormat, String separator) {
         this.format = format;
+        this.javaFormat = javaFormat;
+        this.separator = separator;
     }
 
     public String getFormat() {
         return format;
+    }
+
+    public String getJavaFormat() {
+        return javaFormat;
+    }
+
+    public String getSeparator() {
+        return separator;
     }
 
 }
