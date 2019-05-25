b0953cff10acbd90028b99835c8e2f45123df7dc
George Kovalev <kgp.88@mail.ru>
2019-05-25 14:46:22 +0700

    Test-146241 Selenium: Fix tests with FCKEditor

--- src/main/java/com/onevizion/uitest/api/helper/FckEditor.java
+++ src/main/java/com/onevizion/uitest/api/helper/FckEditor.java
@@ -26,6 +26,9 @@ public class FckEditor {
     private Wait wait;
 
     @Resource
+    private Element element;
+
+    @Resource
     private FckEditorWait fckEditorWait;
 
     public void setValue(String name, String value) {
@@ -35,6 +38,7 @@ public class FckEditor {
 
         wait.waitWebElement(By.id(name));
         wait.waitWebElement(By.id("cke_" + name));
+        element.moveToElementById("cke_" + name);
         fckEditorWait.waitReady(name);
         WebElement div = seleniumSettings.getWebDriver().findElement(By.id("cke_" + name));
         WebElement iframe = div.findElement(By.tagName("iframe"));
@@ -54,24 +58,38 @@ public class FckEditor {
     }
 
     public void checkValue(String name, String expectedValue) {
-      wait.waitWebElement(By.id(name));
-      wait.waitWebElement(By.id("cke_" + name));
-      WebElement div = seleniumSettings.getWebDriver().findElement(By.id("cke_" + name));
-      WebElement iframe = div.findElement(By.tagName("iframe"));
-      seleniumSettings.getWebDriver().switchTo().frame(iframe);
-      String actualValue = seleniumSettings.getWebDriver().findElement(By.tagName("body")).getAttribute("innerHTML").trim();
-      seleniumSettings.getWebDriver().switchTo().parentFrame();
+        if (expectedValue.startsWith("<p>") && expectedValue.endsWith("</p>")) {
+            expectedValue = expectedValue.substring(3, expectedValue.length() - 4);
+        }
 
-      actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_1, AbstractSeleniumCore.SPECIAL_CHARACTERS_1);
-      actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_2, AbstractSeleniumCore.SPECIAL_CHARACTERS_2);
-      actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_3, AbstractSeleniumCore.SPECIAL_CHARACTERS_3);
-      actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_4, AbstractSeleniumCore.SPECIAL_CHARACTERS_4);
+        wait.waitWebElement(By.id(name));
+        wait.waitWebElement(By.id("cke_" + name));
+        element.moveToElementById("cke_" + name);
+        fckEditorWait.waitReady(name);
+        WebElement div = seleniumSettings.getWebDriver().findElement(By.id("cke_" + name));
+        WebElement iframe = div.findElement(By.tagName("iframe"));
+        seleniumSettings.getWebDriver().switchTo().frame(iframe);
+        String actualValue = seleniumSettings.getWebDriver().findElement(By.tagName("body")).getAttribute("innerHTML").trim();
+        seleniumSettings.getWebDriver().switchTo().parentFrame();
 
-      if ("<br>".equals(actualValue) || "<p><br></p>".equals(actualValue)) {
-          actualValue = "";
-      }
+        actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_1, AbstractSeleniumCore.SPECIAL_CHARACTERS_1);
+        actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_2, AbstractSeleniumCore.SPECIAL_CHARACTERS_2);
+        actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_3, AbstractSeleniumCore.SPECIAL_CHARACTERS_3);
+        actualValue = actualValue.replaceAll(AbstractSeleniumCore.SPECIAL_CHARACTERS_ENCODED_4, AbstractSeleniumCore.SPECIAL_CHARACTERS_4);
 
-      Assert.assertEquals(actualValue, expectedValue, "Element with name=[" + name + "] has wrong value");
+        if ("<br>".equals(actualValue) || "<p><br></p>".equals(actualValue)) {
+            actualValue = "";
+        }
+
+        if (actualValue.startsWith("<p>") && actualValue.endsWith("<br></p>")) {
+            actualValue = actualValue.substring(3, actualValue.length() - 8);
+        }
+
+        if (actualValue.startsWith("<p>") && actualValue.endsWith("</p>")) {
+            actualValue = actualValue.substring(3, actualValue.length() - 4);
+        }
+
+        Assert.assertEquals(actualValue, expectedValue, "Element with name=[" + name + "] has wrong value");
     }
 
     public void checkValueReadOnly(String name, String expectedValue) {
@@ -95,15 +113,26 @@ public class FckEditor {
             String prevVal = body.getAttribute("innerHTML");
             Actions actionObject = new Actions(seleniumSettings.getWebDriver());
             for (int i = 0; i < prevVal.length(); i++) {
+                body.click();
                 actionObject.sendKeys(Keys.ARROW_RIGHT).perform();
-            }
-            for (int i = 0; i < prevVal.length(); i++) {
                 actionObject.sendKeys(Keys.BACK_SPACE).perform();
             }
             if (value.length() > 0) {
-                actionObject.sendKeys(value.substring(0, value.length() - 1)).perform();
-                AbstractSeleniumCore.sleep(500L);
-                actionObject.sendKeys(value.substring(value.length() - 1)).perform();
+                //actionObject.sendKeys(value.substring(0, value.length() - 1)).perform();
+                //AbstractSeleniumCore.sleep(500L);
+                //actionObject.sendKeys(value.substring(value.length() - 1)).perform();
+
+                //value = new StringBuilder(value).reverse().toString();
+                //for (int i = 0; i < value.length(); i++) {
+                //    actionObject.sendKeys(Character.toString(value.charAt(i))).perform();
+                //    actionObject.sendKeys(Keys.ARROW_LEFT).perform();
+                //    AbstractSeleniumCore.sleep(100L);
+                //}
+                //value = new StringBuilder(value).reverse().toString();
+
+                actionObject.sendKeys(value).perform();
+                actionObject.sendKeys(" ").perform();
+                actionObject.sendKeys(Keys.BACK_SPACE).perform();
             }
             //body.clear();
             //if (value.length() > 0) {
