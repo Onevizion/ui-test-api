47eb9d65aae9f6f6dbcf16de9959a816a9aeb038
George Kovalev <kgp.88@mail.ru>
2019-10-22 14:01:47 +0700

    Test-150691 Selenium: Increase speed for check HTML select options

---
+++ src/main/java/com/onevizion/uitest/api/vo/HtmlSelectElement.java
@@ -0,0 +1,15 @@
+package com.onevizion.uitest.api.vo;
+
+public class HtmlSelectElement {
+
+    private String label;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+}
