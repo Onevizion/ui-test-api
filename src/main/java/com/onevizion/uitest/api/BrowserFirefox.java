a33c665fbd1c48c0eb5eb9f316d120d5ef235f67
George Kovalev <kgp.88@mail.ru>
2019-11-13 09:15:42 +0700

    Test-149759 Selenium: update firefox from 66 to 69 Try remove FirefoxProfile. https://github.com/mozilla/geckodriver/issues/1623 https://github.com/SeleniumHQ/selenium/issues/7776

--- src/main/java/com/onevizion/uitest/api/BrowserFirefox.java
+++ src/main/java/com/onevizion/uitest/api/BrowserFirefox.java
@@ -3,28 +3,36 @@ package com.onevizion.uitest.api;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.CapabilityType;
 
 public class BrowserFirefox {
 
     public static FirefoxOptions create(SeleniumSettings seleniumSettings) {
-        FirefoxProfile profile = new FirefoxProfile();
-        profile.setPreference("dom.disable_beforeunload", false); //TODO BUG IN W3C https://github.com/w3c/webdriver/issues/1294
-        profile.setPreference("dom.successive_dialog_time_limit", 0);
-        profile.setPreference("dom.max_script_run_time", 1000);
-        profile.setPreference("browser.download.folderList", 2);
-        profile.setPreference("browser.download.dir", seleniumSettings.getUploadFilesPath().replaceAll("\\\\", "\\\\\\\\"));
-        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/xml");
-        //profile.setPreference("security.sandbox.content.level", 5);
-        //profile.setEnableNativeEvents(false); /* TODO selenium 5374 issue */
+//        System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Firefox Nightly\\firefox.exe");
+//        System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\firefox_today_13\\firefox.exe");
+
+//        FirefoxProfile profile = new FirefoxProfile();
+//        profile.setPreference("dom.disable_beforeunload", false); //TODO BUG IN W3C https://github.com/w3c/webdriver/issues/1294
+//        profile.setPreference("dom.successive_dialog_time_limit", 0);
+//        profile.setPreference("dom.max_script_run_time", 1000);
+//        profile.setPreference("browser.download.folderList", 2);
+//        profile.setPreference("browser.download.dir", seleniumSettings.getUploadFilesPath().replaceAll("\\\\", "\\\\\\\\"));
+//        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/xml");
+//        //profile.setPreference("security.sandbox.content.level", 5);
+//        //profile.setEnableNativeEvents(false); /* TODO selenium 5374 issue */
 
         FirefoxOptions options = new FirefoxOptions();
-        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+//        options.setProfile(profile);
+        options.addPreference("dom.disable_beforeunload", false); //TODO BUG IN W3C https://github.com/w3c/webdriver/issues/1294
+        options.addPreference("dom.successive_dialog_time_limit", 0);
+        options.addPreference("dom.max_script_run_time", 1000);
+        options.addPreference("browser.download.folderList", 2);
+        options.addPreference("browser.download.dir", seleniumSettings.getUploadFilesPath().replaceAll("\\\\", "\\\\\\\\"));
+        options.addPreference("browser.helperApps.neverAsk.saveToDisk", "text/xml");
         //options.addPreference("security.sandbox.content.level", 5);
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
         //options.setLegacy(true);
         //options.setLogLevel(FirefoxDriverLogLevel.TRACE);
-        options.setProfile(profile);
         //TODO https://github.com/mozilla/geckodriver/issues/617
         //https://bugzilla.mozilla.org/show_bug.cgi?id=1264259
         options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.IGNORE);
