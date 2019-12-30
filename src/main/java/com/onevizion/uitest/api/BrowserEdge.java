22f696450a5e70dc69ddc06de416334e5ec59253
George Kovalev <kgp.88@mail.ru>
2019-12-30 13:25:09 +0700

    Test-151258 Selenium: Support edge

--- src/main/java/com/onevizion/uitest/api/BrowserEdge.java
+++ src/main/java/com/onevizion/uitest/api/BrowserEdge.java
@@ -1,24 +1,30 @@
 package com.onevizion.uitest.api;
 
+import java.util.HashMap;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.remote.CapabilityType;
+
 public class BrowserEdge {
 
     public static CustomEdgeOptions create(SeleniumSettings seleniumSettings) {
-//        //https://stackoverflow.com/questions/58590613/msedgedriver-chromium-does-not-work-after-version-79-0-309-0
-//
-//        System.setProperty("webdriver.edge.driver", "C:\\workspace2\\msedgedriver.exe");
-//        //System.setProperty("webdriver.edge.bin", "C:\\Program Files (x86)\\Microsoft\\Edge Dev\\Application\\msedge.exe");
-//        //System.setProperty("webdriver.edge.bin", "C:\\Users\\Администратор\\AppData\\Local\\Microsoft\\Edge SxS\\Application\\msedge.exe");
-//
-//        ChromeOptions chromeOptions = new ChromeOptions();
-//        //chromeOptions.setBinary("C:\\Program Files (x86)\\Microsoft\\Edge Dev\\Application\\msedge.exe");
-//        chromeOptions.setBinary("C:\\Users\\Администратор\\AppData\\Local\\Microsoft\\Edge SxS\\Application\\msedge.exe");
-//        EdgeOptions options = new EdgeOptions();
-//        options.merge(chromeOptions);
-//
-//        return options;
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("safebrowsing.enabled", "true");
+        chromePrefs.put("download.default_directory", seleniumSettings.getUploadFilesPath());
+        HashMap<String, Object> chromeDownloadPrefs = new HashMap<>();
+        chromeDownloadPrefs.put("default_directory", seleniumSettings.getUploadFilesPath());
+        chromePrefs.put("download", chromeDownloadPrefs);
 
         CustomEdgeOptions options = new CustomEdgeOptions();
         options.setBinary("C:\\Users\\Administrator\\AppData\\Local\\Microsoft\\Edge SxS\\Application\\msedge.exe");
+
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.IGNORE);
+        options.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+        options.setCapability(CapabilityType.UNHANDLED_PROMPT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
 
         if (seleniumSettings.getHeadlessMode()) {
             options.addArguments("--headless");
