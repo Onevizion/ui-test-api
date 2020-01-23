ae806517d65d9de3fd416f3d78a1e4c977d049c1
George Kovalev <kgp.88@mail.ru>
2020-01-23 10:17:27 +0700

    Test-153416 Selenium: API V3: Update tests for "chats" - subscribers

---
+++ src/main/java/com/onevizion/uitest/api/vo/api/v3/ChatSubscriber.java
@@ -0,0 +1,60 @@
+package com.onevizion.uitest.api.vo.api.v3;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ChatSubscriber implements Comparable<ChatSubscriber> {
+
+    private String userId;
+    private String userName;
+
+    @JsonProperty("user_id")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @JsonProperty("user_name")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public int compareTo(ChatSubscriber o) {
+        return this.userId.compareTo(o.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, userName);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ChatSubscriber other = (ChatSubscriber) obj;
+        return Objects.equals(userId, other.userId) && Objects.equals(userName, other.userName);
+    }
+
+    @Override
+    public String toString() {
+        return "ChatSubscriber [userId=" + userId + ", userName=" + userName + "]";
+    }
+
+}
