4c79af2586e23029ef97b0000cd004e6386b510a
Julia Tokmagasheva <tokmagasheva@mail.ru>
2020-02-28 11:42:45 +0700

    Test-155276 Selenium: Check screenshot on MAPPER page

---
+++ src/main/java/com/onevizion/uitest/api/helper/mapper/MapperWait.java
@@ -0,0 +1,25 @@
+package com.onevizion.uitest.api.helper.mapper;
+
+import javax.annotation.Resource;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.stereotype.Component;
+
+import com.onevizion.uitest.api.SeleniumSettings;
+
+@Component
+public class MapperWait {
+
+    @Resource
+    private SeleniumSettings seleniumSettings;
+
+    @Resource
+    private MapperJs mapperJs;
+
+    public void waitIsMapLoaded() {
+        new WebDriverWait(seleniumSettings.getWebDriver(), seleniumSettings.getDefaultTimeout())
+            .withMessage("Waiting for Map loading is failed")
+            .until(webdriver -> mapperJs.isMapLoaded());
+    }
+
+}
