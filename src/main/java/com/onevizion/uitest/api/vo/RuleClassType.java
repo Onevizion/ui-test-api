37e72f217fb63265d4f4c1bf6b9340e23f629427
George Kovalev <kgp.88@mail.ru>
2020-06-30 13:19:22 +0700

    Test-161532 Selenium: Remove ArcGis related rule classes

--- src/main/java/com/onevizion/uitest/api/vo/RuleClassType.java
+++ src/main/java/com/onevizion/uitest/api/vo/RuleClassType.java
@@ -10,8 +10,6 @@ public enum RuleClassType {
     FORWARD_GEOCODING("Forward Geocoding"),
     TOWER_DIAGRAM("Tower Diagram"),
     MAP_SNIPPET("Map Snippet"),
-    GEOCODING_ARCGIS("Geocoding ArcGIS"),
-    FORWARD_GEOCODING_ARCGIS("Forward Geocoding ArcGIS"),
     TOWER_PROFILE("Tower Profile");
 
     private String name;
