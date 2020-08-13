28bd07950a4d0d9f4055e30701ada81d7ece2b43
George Kovalev <kgp.88@mail.ru>
2020-08-13 16:00:48 +0700

    Test-164642 Selenium: Allow grid buttons and applets to be pinned by User calendars grid on admin wp page remove wait.waitFormLoad(); may be it is not needed

--- src/main/java/com/onevizion/uitest/api/helper/grid/button/GridButton.java
+++ src/main/java/com/onevizion/uitest/api/helper/grid/button/GridButton.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.onevizion.uitest.api.AbstractSeleniumCore;
 import com.onevizion.uitest.api.SeleniumSettings;
+import com.onevizion.uitest.api.helper.Element;
 import com.onevizion.uitest.api.helper.ElementWait;
 import com.onevizion.uitest.api.helper.Wait;
 import com.onevizion.uitest.api.helper.Window;
@@ -19,6 +20,8 @@ public class GridButton {
 
     private static final String BUTTON_GRID_ROW_EDITOR_ID_BASE = "itemEditRow";
     private static final String BUTTON_SHOW_SQL_ID_BASE = "itemSQL";
+    private static final String BUTTON_COMPONENT_EXPORT_ID_BASE = "itemExportRun";
+    private static final String BUTTON_COMPONENT_IMPORT_ID_BASE = "itemImportRun";
     private static final String BUTTON_COLORS_ID_BASE = "itemColors";
     private static final String BUTTON_COORDINATES_ID_BASE = "itemCoordLinks";
     private static final String BUTTON_VALIDATIONS_ID_BASE = "itemValidation";
@@ -42,6 +45,9 @@ public class GridButton {
     @Autowired
     private Wait wait;
 
+    @Autowired
+    private Element element;
+
     public void openGridRowEditorForm(Long gridIdx) {
         openOptionsPanel(gridIdx);
 
@@ -64,6 +70,30 @@ public class GridButton {
         window.openModal(By.id(BUTTON_SHOW_SQL_ID_BASE + gridIdx));
         wait.waitWebElement(By.id(AbstractSeleniumCore.BUTTON_CANCEL_ID_BASE));
         wait.waitFormLoad();
+    }
+
+    public void openComponentExportForm(Long gridIdx) {
+        openOptionsPanel(gridIdx);
+
+        elementWait.waitElementById(BUTTON_COMPONENT_EXPORT_ID_BASE + gridIdx);
+        elementWait.waitElementVisibleById(BUTTON_COMPONENT_EXPORT_ID_BASE + gridIdx);
+        elementWait.waitElementDisplayById(BUTTON_COMPONENT_EXPORT_ID_BASE + gridIdx);
+
+        window.openModal(By.id(BUTTON_COMPONENT_EXPORT_ID_BASE + gridIdx));
+        wait.waitReloadForm("form=1");
+        wait.waitWebElement(By.id(AbstractSeleniumCore.BUTTON_OK_ID_BASE));
+        wait.waitFormLoad();
+    }
+
+    public void clickComponentImport(Long gridIdx) {
+        openOptionsPanel(gridIdx);
+
+        elementWait.waitElementById(BUTTON_COMPONENT_IMPORT_ID_BASE + gridIdx);
+        elementWait.waitElementVisibleById(BUTTON_COMPONENT_IMPORT_ID_BASE + gridIdx);
+        elementWait.waitElementDisplayById(BUTTON_COMPONENT_IMPORT_ID_BASE + gridIdx);
+
+        element.clickById(BUTTON_COMPONENT_IMPORT_ID_BASE + gridIdx);
+        grid2.waitLoad(gridIdx);
     }
 
     public void openColorsGrid(Long gridIdx) {
