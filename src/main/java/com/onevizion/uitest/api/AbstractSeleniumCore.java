2496422bf22e836392c903107565152a2d83d068
George Kovalev <kgp.88@mail.ru>
2020-08-17 10:39:14 +0700

    Test-164642 Selenium: Allow grid buttons and applets to be pinned by User

--- src/main/java/com/onevizion/uitest/api/AbstractSeleniumCore.java
+++ src/main/java/com/onevizion/uitest/api/AbstractSeleniumCore.java
@@ -453,8 +453,6 @@ public abstract class AbstractSeleniumCore extends AbstractTestNGSpringContextTe
 
     public static final String BUTTON_ADD_TREE_ID_BASE = "btnAddTree";
     public static final String BUTTON_EDIT_TREE_ID_BASE = "btnEditTree";
-    public static final String BUTTON_UP_TREE_ID_BASE = "btnUpTree";
-    public static final String BUTTON_DOWN_TREE_ID_BASE = "btnDownTree";
 
     public static final String PREFIX_LOCAL = "L:";
     public static final String PREFIX_GLOBAL = "G:";
