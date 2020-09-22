9d245b21112beffc65e2d4bc20afa09359fc9a73
Ivan Larionov <xlebnica.booroop@gmail.com>
2020-09-22 15:35:28 +0700

    Test-168312 Selenium: API V3: Create tests for "filters"

--- src/main/java/com/onevizion/uitest/api/vo/ApiV3EndpointType.java
+++ src/main/java/com/onevizion/uitest/api/vo/ApiV3EndpointType.java
@@ -18,7 +18,7 @@ public enum ApiV3EndpointType {
     FIELD_COLORS_READ_COLOR_BY_TID("GET", "/v3/trackors/{trackor_id}/fields/colors", "Read Field Colors by Trackor id"),
     FIELD_COLORS_UPDATE("PUT", "/v3/trackors/{trackor_id}/fields/{field_name}/colors", "Update Field Color by Trackor ID and Field name"),
 
-    FILTERS_READ_FILTERS_BY_TT("GET", "/v3/trackor_types/{trackor_type}/filters", "Read filters by Trackor Type"),
+    FILTERS_READ_FILTERS("GET", "/v3/trackor_types/{trackor_type}/filters", "Read filters by Trackor Type"),
     FILTERS_READ_FILTER_CONDITIONS("GET", "/v3/trackor_types/{trackor_type}/filters/{filter_name}", "Read filter conditions"),
     FILTERS_SET_FILTER_AS_CURRENT("POST", "/v3/trackor_types/{trackor_type}/filters/{filter_name}/set_as_current", "Set filter as current"),
     
